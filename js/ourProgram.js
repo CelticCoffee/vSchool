3892a1c82323e3f8b64caf43aa4090e0c1a24ea5
Wendy <wendygoodwin@mac.com>
2015-09-11 14:36:06 -0700

    working jQuery on ourProgram

---
+++ js/ourProgram.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+	$('#musicButton').click(function(event) {
+		$('#musicText').toggle('slow');
+		console.log('snowman');
+	});
+
+	 $('#ensembleButton').click(function(event) {
+		$('#musicEnsembles').toggle('slow');
+		console.log('fish');
+	});
+
+	 $('#peButton').click(function(event) {
+		$('#peText').toggle('slow');
+		console.log('snowman');
+	});
+
+	 $('#uniformButton').click(function(event) {
+		$('#peUniform').toggle('slow');
+		console.log('boots');
+	});
+
+
+	 $('#artButton').click(function(event) {
+		$('#artText').toggle('slow');
+		console.log('snowman');
+	});
+
+	 $('#artShowButton').click(function(event) {
+		$('#artShowText').toggle('slow');
+		console.log('boots');
+	});
+
+	 $('#libraryButton').click(function(event) {
+		$('#libraryText').toggle('slow');
+		console.log('boat');
+	});
+
+	
+
+});
