ddcba38626ecb15d427e360367c521921cf9660c
Wendy <wendygoodwin@mac.com>
2015-09-17 21:16:10 -0700

    links js

--- js/academics.js
+++ js/academics.js
@@ -14,32 +14,39 @@ $(document).ready(function() {
 	$('#kindergartenLink').click(function() {
 		$('#kindergartenLink').addClass('active').siblings().removeClass('active');
 		$('#kindergarten').addClass('showText').siblings().removeClass('showText');
+		$('#faculty').addClass('noShowText');
 	});
 
 	$('#firstGradeLink').click(function() {
 		$('#firstGradeLink').addClass('active').siblings().removeClass('active');
 		$('#firstGrade').addClass('showText').siblings().removeClass('showText');
+		$('#faculty').addClass('noShowText');
 	});
 
 	$('#secondGradeLink').click(function() {
 		$('#secondGradeLink').addClass('active').siblings().removeClass('active');
 		$('#secondGrade').addClass('showText').siblings().removeClass('showText');
+		$('#faculty').addClass('noShowText');
 	});
 	$('#thirdGradeLink').click(function() {
 		$('#thirdGradeLink').addClass('active').siblings().removeClass('active');
 		$('#thirdGrade').addClass('showText').siblings().removeClass('showText');
+		$('#faculty').addClass('noShowText');
 	});
 	$('#fourthGradeLink').click(function() {
 		$('#fourthGradeLink').addClass('active').siblings().removeClass('active');
 		$('#fourthGrade').addClass('showText').siblings().removeClass('showText');
+		$('#faculty').addClass('noShowText');
 	});
 	$('#fifthGradeLink').click(function() {
 		$('#fifthGradeLink').addClass('active').siblings().removeClass('active');
 		$('#fifthGrade').addClass('showText').siblings().removeClass('showText');
+		$('#faculty').addClass('noShowText');
 	});
 	$('#middleSchoolLink').click(function() {
 		$('#middleSchoolLink').addClass('active').siblings().removeClass('active');
 		$('#middleSchool').addClass('showText').siblings().removeClass('showText');
+		$('#faculty').addClass('noShowText');
 	});
 });
 
