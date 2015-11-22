7080b3a9df2ff825994131e91408fb7a0318348c
Wendy <wendygoodwin@mac.com>
2015-11-22 11:28:31 -0800

    setting up server

---
+++ app/public/javascripts/admissions.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	console.log('hello Cathedral');
+
+	$("#admissionsButton").click(function() {
+		$('.jumbotron').toggle();
+	});
+
+	$("#backButton").click(function() {
+		$('.jumbotron').toggle();
+	});
+	$
+
+	$('#requestInfoButton').click(function() {
+		$('form').toggle();
+	});
+});
+/******************************************/
+/*************Inquiry Form ***************/
+/*****************************************/
---
+++ app/public/javascripts/ourProgram.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+	console.log('ourprogram is ready');
+	$('#musicButton').click(function() {
+		$('#musicText').toggle('slow');
+	});
+
+	 $('#ensembleButton').click(function(event) {
+		$('#musicEnsembles').toggle('slow');
+		console.log('fish');
+	});
+
+	 $('#peButton').click(function(event) {
+		$('#peText').toggle('slow');
+		console.log('snowman');
+	});
+
+	 $('#uniformButton').click(function(event) {
+		$('#peUniform').toggle('slow');
+		console.log('boots');
+	});
+
+
+	 $('#artButton').click(function(event) {
+		$('#artText').toggle('slow');
+		console.log('snowman');
+	});
+
+	 $('#artShowButton').click(function(event) {
+		$('#artShowText').toggle('slow');
+		console.log('boots');
+	});
+
+	 $('#libraryButton').click(function(event) {
+		$('#libraryText').toggle('slow');
+		console.log('boat');
+	});
+
+	 $('#spanishButton').click(function(event) {
+		$('#spanishText').toggle('slow');
+		console.log('snow');
+	});
+
+	  $('#techButton').click(function(event) {
+		$('#techText').toggle('slow');
+		console.log('pig');
+	});
+
+	$('#activitiesButton').click(function(event) {
+		$('#cyoText').toggle('slow');
+		console.log('snow');
+	});
+
+	$('#extendedCareButton').click(function(event) {
+		$('#afterCareText').toggle('slow');
+		console.log('snow');
+	});
+});
