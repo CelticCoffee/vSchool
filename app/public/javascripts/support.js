075a6519e33dd687bae9f3f718c3d76331a6a4ae
Wendy <wendygoodwin@mac.com>
2016-01-25 10:39:00 -0800

    supportLink Done

--- app/public/javascripts/support.js
+++ app/public/javascripts/support.js
@@ -19,4 +19,31 @@ $(document).ready(function() {
   $('#run4Sponsors').click(function() {
     $('.run4More').toggle();
   });
+
+
+  if(screen.width < 415) {
+    $('#iconMenu').remove();
+    $('#supportTabs').show();
+    console.log('bacon');
+  }
+  else if (screen.width >=415) {
+    console.log('baconToast');
+    $('#supportTabs').remove();
+  };
+
+  $('#legacyTab').click(function() {
+    $('#legacy').show().siblings().addClass('noShowText');
+    console.log('baconEggs');
+  });
+
+  $('#aucTab').click(function() {
+    $('#cathAucHero').show().siblings().addClass('noShowText');
+    console.log('tomato');
+  });
+
+  $('#run4Tab').click(function() {
+    $('#run4CathedralHero').show().siblings().addClass('noShowText');
+    console.log('snail');
+  });
+
 });
