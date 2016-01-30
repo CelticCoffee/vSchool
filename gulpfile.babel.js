f70695ebcad25934a17b80922a17265665311834
Wendy <wendygoodwin@mac.com>
2016-01-30 04:13:40 -0800

    v3 code replacement

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,14 @@
-//$use strict//
 
+var gulp = require('gulp'),
+    useref = require('gulp-useref');
 
 gulp.task('default', function () {
     return gulp.src('app/*.html')
         .pipe(useref())
         .pipe(gulp.dest('dist'));
 });
+
+
 
 gulp.task('deploy', function () {
   return gulp.src("./dist/**/*")
